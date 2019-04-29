c417f8cb4e4e23a41e87675d10b0e25aeb88c5d4
Nitin Gaba <>
2019-04-29 11:45:56 +0530

    changes

--- src/app/data.ts
+++
@@ -1,9 +0,0 @@
-export class Data {
-  "title": string;
-  "scheduleDate": string;
-  "locale": string;
-  "price": string;
-  "status": string;
-  "daysDiff_en": string;
-  "daysDiff_fr": string;
-}
